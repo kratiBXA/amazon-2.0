2195e4e89d0d2b143eb4bbe220d15290caf0ea35
kratiBXA <krati.gupta@bounteous.com>
2025-08-29 18:41:12 +0530

    Update Amazon 2.0

---
+++ src/app/components/productg.tsx
@@ -0,0 +1,13 @@
+// // import ProductCard from '../components/ProductCard';  // Correct 
+// import ProductCard from '@components/ProductCard'; // Using absolute import
+
+
+// export default function ProductGrid({ products }: { products: any[] }) {
+//   return (
+//     <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))', gap: '1rem' }}>
+//       {products.map(product => (
+//         <ProductCard key={product.id} product={product} />
+//       ))}
+//     </div>
+//   );
+// }
